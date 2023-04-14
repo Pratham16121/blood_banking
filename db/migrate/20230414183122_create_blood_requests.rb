6ac19acd57fd8da496d3221c8543faa37a74ba78
Nandini Jhanwar <nandinijhanwar67@gmail.com>
2023-04-15 00:19:03 +0530

    Set default value of is_completed attribute in BloodRequest to false

--- db/migrate/20230414183122_create_blood_requests.rb
+++ db/migrate/20230414183122_create_blood_requests.rb
@@ -3,7 +3,7 @@ class CreateBloodRequests < ActiveRecord::Migration[7.0]
     create_table :blood_requests do |t|
       t.integer :recipent_id, foreign_key: {to_table: :users}
       t.integer :blood_type
-      t.boolean :is_completed
+      t.boolean :is_completed, default: :false
 
       t.timestamps
     end
