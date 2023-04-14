c0d9b1bf44f348a58e6065765db114d146c78adc
Pratham Goel <pratham.goel16@gmail.com>
2023-04-15 00:26:15 +0530

    Changed the name of the before_create function

--- app/models/blood_bag.rb
+++ app/models/blood_bag.rb
@@ -1,8 +1,8 @@
 class BloodBag < ApplicationRecord
-  before_create :set_expiry
+  before_create :set_expiry_date
   enum blood_type: User.blood_types
 
-  def set_expiry
+  def set_expiry_date
     self.expiry_date = Time.now + 42.days
   end
 
