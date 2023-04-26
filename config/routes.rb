6c0daeaf175a2cdd2c81ac183e270a8d47943262
Pratham Goel <pratham.goel16@gmail.com>
2023-04-26 13:55:41 +0530

    Added routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,27 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  api_version(:module => "Api::V1", :header => {
-    name: "Accept", :value => "application/vnd.blood_banking; version=1"}) do
-      resources :users, only: [ :index, :create ] do 
-        collection do
-          post 'login'
-        end
-      end
-      resources :blood_requests, only: [ :index, :create, :update ]
-      resources :blood_banks, only: [ :index, :create ]
-      resources :donations, only: [ :create ]
+  root 'users#root'
+  resources :users, only: [ :index, :create ] do 
+    collection do
+      post 'login'
+      get 'new', as: 'new'
+      get 'search'
+    end
+  end
+  resources :blood_requests, only: [ :index, :create, :update ] do
+    collection do
+      get 'new', as: 'new'
+    end
+  end
+  resources :blood_banks, only: [ :index, :create ] do
+    collection do
+      get 'new', as: 'new'
+    end
+  end
+  resources :donations, only: [ :create ] do
+    collection do
+      get 'new', as: 'new'
+    end
   end
 end
